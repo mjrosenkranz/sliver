8daba3b4eb522ff06cadcfa1aa92c5a11d80eb0f
Morgan Rosenkranz <rosenkranz1999@gmail.com>
2023-10-05 23:15:53 -0400

    messing around with references for build type

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,59 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::rc::Rc;
+
+#[derive(Debug)]
+struct Bounds (f32, f32, f32);
+
+struct World {
+    geoms: Vec<Geometric>
 }
+
+struct Build<'a> {
+    bounds: Bounds,
+    parts: Vec<Part<'a>>,
+}
+
+struct Part<'a> {
+    geoms: Vec<&'a Geometric>
+}
+
+#[derive(Debug)]
+struct Geometric {
+    bounds: Bounds
+}
+
+impl Geometric {
+    
+    fn new() -> Self {
+        Geometric{
+            bounds: Bounds(0., 0., 0.),
+        }
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test() {
+        let mut source: Vec<Box<Geometric>> = vec!(
+            Box::new(Geometric::new()),
+        );
+
+
+        let mut part = Part {
+            geoms: vec!()
+        };
+
+        for s in source.iter() {
+            part.geoms.push(&**s);
+        }
+
+        // stops you from doing this!
+        // source.remove(0);
+
+        println!("{:?}", part.geoms[0]);
     }
+
 }
